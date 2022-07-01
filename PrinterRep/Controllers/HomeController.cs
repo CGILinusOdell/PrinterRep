27c9de24e3d7476bf2d547936156009724c429f5
GodWalksWithHer <malin.widen@cgi.com>
2022-07-01 16:08:03 +0200

    Sida Add - lägga till skrivare

--- PrinterRep/Controllers/HomeController.cs
+++ PrinterRep/Controllers/HomeController.cs
@@ -32,6 +32,10 @@ namespace PrinterRep.Controllers
         {
             return View();
         }
+        public IActionResult Add()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
